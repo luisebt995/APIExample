3c5616b88e5863bab386ca85a4ddccd3942fdd19
Luis Bustamante <luisebt995@gmail.com>
2024-05-03 17:57:16 +0200

    Initial Commit

---
+++ APIExample/APIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIExampleApp.swift
+//  APIExample
+//
+//  Created by Luis on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct APIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APIExample
+//
+//  Created by Luis on 3/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
